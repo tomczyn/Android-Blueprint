4e116100452c4b515cd80846d1e1d2093a86b3ed
tomczyn <dev@tomczyn.com>
2023-02-16 14:17:06 +0100

    Better checkReleaseVersion message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,9 +82,9 @@ val checkReleaseVersion by tasks.registering {
         val versionName = android.defaultConfig.versionName
         if (versionName?.matches("\\d+(\\.\\d+)+".toRegex()) == false) {
             throw GradleException(
-                "Version name for release builds can only be numeric (like 1.0.0), but was $versionName\n" +
+                "Version name for release builds can only be numeric (like 1.0), but was $versionName\n" +
                         "Please use git tag to set version name on the current commit and try again\n" +
-                        "For example: git tag -a 1.0.0 -m 'tag message'"
+                        "For example: git tag -a 1.0 -m 'v1.0'"
             )
         }
     }
